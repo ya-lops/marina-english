1ebd04538c66e239cfb21226296373cf51cf1d36
Vitaliy L <lops@list.ru>
2026-08-20 20:36:23 +0300

    draft

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,26 +10,21 @@ export default defineConfig({
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
-			provider: fontProviders.local(),
-			name: 'Atkinson',
-			cssVariable: '--font-atkinson',
-			fallbacks: ['sans-serif'],
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
-						weight: 400,
-						style: 'normal',
-						display: 'swap',
-					},
-					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
-						weight: 700,
-						style: 'normal',
-						display: 'swap',
-					},
-				],
-			},
-		},
+      provider: fontProviders.google(),
+      name: "Montserrat",
+      cssVariable: "--site-font-base",
+      subsets: ["latin", "cyrillic"],
+      weights: [400, 500, 600],
+      styles: ["normal"],
+      fallbacks: [
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "Segoe UI",
+        "Roboto",
+        "Helvetica Neue",
+        "Arial",
+        "sans-serif",
+      ],
+    },
 	],
 });
--- src/content.config.ts
+++ src/content.config.ts
@@ -2,19 +2,15 @@ import { defineCollection } from 'astro:content';
 import { glob } from 'astro/loaders';
 import { z } from 'astro/zod';
 
-const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
+const lessons = defineCollection({
+	loader: glob({ base: './src/content/lessons', pattern: '**/*.{md,mdx}' }),
 	schema: ({ image }) =>
 		z.object({
 			title: z.string(),
 			description: z.string(),
-			// Transform string to Date object
-			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
 			heroImage: z.optional(image()),
 		}),
 });
 
-export const collections = { blog };
+export const collections = { lessons };
